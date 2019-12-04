3f9a652c1157f8a9d560cf34e10cf2b5a3e2ed4a
Ben Ely <ben.ely@h4bmanchester.com>
2019-12-04 13:55:23 +0000

    Part 2 of day 2

--- src/day-2.js
+++ src/day-2.js
@@ -1,10 +1,12 @@
 export function intcode(pattern) {
+    pattern = pattern.slice();
+
     for (let idx = 0; idx < pattern.length; idx += 4) {
         if (pattern[idx] === 1) {
-            pattern[pattern[idx + 3]] = pattern[pattern[idx + 1]] + pattern[pattern[idx + 2]];
+            pattern[pattern[(idx + 3)]] = pattern[pattern[idx + 1]] + pattern[pattern[idx + 2]];
         }
         else if (pattern[idx] === 2) {
-            pattern[pattern[idx + 3]] = pattern[pattern[idx + 1]] * pattern[pattern[idx + 2]];
+            pattern[pattern[(idx + 3)]] = pattern[pattern[idx + 1]] * pattern[pattern[idx + 2]];
         }
         else if (pattern[idx] === 99) {
             // exit intcode
@@ -13,4 +15,19 @@ export function intcode(pattern) {
     }
 
     return pattern;
+}
+
+export function findNounVerbPair(pattern, target) {
+    for (let noun = 0; noun <= 99; noun++) {
+        for (let verb = 0; verb <= 99; verb++) {
+            pattern[1] = noun;
+            pattern[2] = verb;
+
+            if (intcode(pattern)[0] === target) {
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    return false;
 }
